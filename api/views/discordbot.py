acce4aa1c44ab28b334c2d140b17fa01a89922bf
kota <suzuki.kota0331@gmail.com>
2020-05-01 10:09:08 +0900

    discordbot apply blueprint

--- api/views/discordbot.py
+++ api/views/discordbot.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, jsonify
 import discord
 import re
 import datetime
@@ -38,6 +38,27 @@ async def on_ready():
 	print(
 		f'{client.user} is connected to the following guild:\n'
 		f'{guild.name}(id: {guild.id})\n')
+
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+	# メッセージ送信者がBotだった場合は無視する
+	if message.author.bot:
+		return
+
+	# guildを指定
+	for guild in client.guilds:
+		if guild.name == GUILD:
+			break
+
+	# チャンネル名を指定して消す
+	if message.content.startswith("/delete_channel"):
+		channel_name = message.content.split()[1]
+		channel = discord.utils.get(guild.channels, name=channel_name)
+		await channel.delete()
+		reply = f'{channel_name} を削除しました'
+		await message.channel.send(reply)
 
 
 # memberのボイスステータスがかわると起動
@@ -81,8 +102,7 @@ client.run(TOKEN)
 # フリーボイスからの移動か招待URLを送る
 # またサーバーミュートを消す
 # (cim = create invite move)
-@discordbot.route('/cim_voice', methods=["GET", "POST"])
-def cim_voice(name):
+def ft_cim_voice(name):
 
 	# guildを指定
 	for guild in client.guilds:
@@ -103,29 +123,28 @@ def cim_voice(name):
 	# memberがFREE_VOICE_CHANNELにいるかどうかで場合分け
 	# free_voice → チャンネルをに移動
 	# not free_voice → DMで招待URLを送る
+	reply = 0
 	flag = True
 	if member.voice:
 		if member.voice.channel.name == FREE_VOICE_CHANNEL:
 			member.edit(mute=False, voice_channel=new_channel)
-			# reply = f'{new_channel.mention} の作成とmemberの移動をしました'
-			reply = '作成と移動が完了した旨のフラグ'
+			# '作成と移動が完了した旨のフラグ'
+			reply = 1
 			flag = False
 	if flag:
 		invite = new_channel.create_invite()
 		if not member.dm_channel:
 			member.create_dm()
 		member.dm_channel.send(invite)
-		# reply = f'{new_channel.mention} の作成と招待をしました'
-		reply = '作成と招待が完了した旨のフラグ'
-	# message.channel.send(reply)
+		# '作成と招待が完了した旨のフラグ'
+		reply = 2
 	return reply
 
 
 # name1 の人が name2の人の席をクリックして
 # name2の人のチャンネルの状況により
 # ボイスチャンネルを作ったり、招待したりする
-@discordbot.route('/cim_two_voice', methods=["GET", "POST"])
-def cim_two_voice(name1, name2):
+def ft_cim_two_voice(name1, name2):
 
 	# guildを指定
 	for guild in client.guilds:
@@ -153,21 +172,22 @@ def cim_two_voice(name1, name2):
 		channel_name = name1 + '_{0:%d%H%M%S}'.format(now)
 		channel = category.create_voice_channel(name=channel_name)
 
+	reply = [0, 0]
 	# member1の処理
 	flag = True
 	if member1.voice:
 		if member1.voice.channel.name == FREE_VOICE_CHANNEL:
 			member1.edit(mute=False, voice_channel=channel)
-			# reply = f'{channel.mention} の作成とmemberの移動をしました'
-			reply = 'member1を移動したフラグ'
+			# 'member1を移動したフラグ'
+			reply[0] = 1
 			flag = False
 	if flag:
 		invite = channel.create_invite()
 		if not member1.dm_channel:
 			member1.create_dm()
 		member1.dm_channel.send(invite)
-		# reply = f'{channel.mention} の作成と招待をしました'
-		reply = 'member1にDMを送ったフラグ'
+		# 'member1にDMを送ったフラグ'
+		reply[0] = 2
 
 	# member2の処理
 	if flag_make_channel:
@@ -175,23 +195,23 @@ def cim_two_voice(name1, name2):
 		if member2.voice:
 			if member2.voice.channel.name == FREE_VOICE_CHANNEL:
 				member2.edit(mute=False, voice_channel=channel)
-				# reply += f'{channel.mention} の作成とmemberの移動をしました'
-				reply += 'member1を移動したフラグ'
+				# 'member1を移動したフラグ'
+				reply[1] = 1
 				flag = False
 		if flag:
 			invite = channel.create_invite()
 			if not member2.dm_channel:
 				member2.create_dm()
 			member2.dm_channel.send(invite)
-			# reply += f'{channel.mention} の作成と招待をしました'
-			reply = 'member2にDMを送ったフラグ'
-	# message.channel.send(reply)
+			'member2にDMを送ったフラグ'
+			reply[1] = 2
+	else:
+		reply[1] = -1
 	return reply
 
 
 # memberを指定してログイン状態を返す
-@discordbot.route('/status', methods=["GET", "POST"])
-def return_status(name):
+def ft_return_status(name):
 
 	# guildを指定
 	for guild in client.guilds:
@@ -202,18 +222,16 @@ def return_status(name):
 	member = discord.utils.find(lambda m: match_name(name, m.name), guild.members)
 
 	if str(member.status) == 'online':
-		# reply = f'{member.name} はオンラインです'
-		reply = 'memberがオンラインであることを表すフラグ'
+		# 'memberがオンラインであることを表すフラグ'
+		reply = 1
 	else:
-		# reply = f'{member.name} はオフラインです'
-		reply = 'memberがオンラインではないことをしめすフラグ'
-	# message.channel.send(reply)
+		# 'memberがオンラインではないことをしめすフラグ'
+		reply = 0
 	return reply
 
 
 # フリーボイスに移動させてサーバーミュートにする
-@discordbot.route('/move_freevoice', methods=["GET", "POST"])
-def move_to_fv(name):
+def ft_move_freevoice(name):
 
 	# guildを指定
 	for guild in client.guilds:
@@ -227,51 +245,115 @@ def move_to_fv(name):
 	channel = discord.utils.get(guild.channels, name=FREE_VOICE_CHANNEL)
 
 	# memberをfreevoiceチャンネルに移動する
-	flag = True
+	reply = 0
 	if member.voice:
 		if member.voice.channel:
 			member.edit(mute=True, voice_channel=channel)
-			# reply = f'{member.name} をFreeVoiceに移動しました'
-			reply = 'freevoiceへの移動が完了したフラグ'
-			flag = False
-	if flag:
-		# reply = f'{member.name} Voiceチャンネルにいません'
-		reply = 'freevoiceへの移動ができなかったフラグ'
-	# message.channel.send(reply)
+			# 'freevoiceへの移動が完了したフラグ'
+			reply = 1
 	return reply
-
-
-# 特定のカテゴリーの空のチャンネルを削除する
-@discordbot.route('/delete_channel', methods=["GET", "POST"])
-def delete_channel():
-
-	# guildを指定
-	for guild in client.guilds:
-		if guild.name == GUILD:
-			break
-
-	# カテゴリーを指定
-	category = discord.utils.get(guild.categories, name=VOICE_CATEGORY)
-
-	# 上記カテゴリー内のmemberがいないチャンネルを削除
-	for channel in [c for c in category.channels if not c.members]:
-		channel.delete()
 
 
 # 
 # 以下Blueprintについて
 # 
 
+
 # Blueprintのオブジェクトを生成する
 discordbot = Blueprint('discordbot', __name__)
 
 
 @discordbot.route('/cim_voice', methods=["GET", "POST"])
+@jwt_required()
+def cim_voice():
+	flag = ft_cim_voice(current_identity.id)
+	if flag:
+		if flag == 1:
+			return make_response(jsonify({
+				'flag': '作成と移動が完了した旨のフラグ'
+			}))
+		else:
+			return make_response(jsonify({
+				'flag': '作成と招待が完了した旨のフラグ'
+			}))
+	else:
+		return make_response(jsonify({
+			'flag': 'エラーが起こった旨のフラグ'
+		}))
+
 
 @discordbot.route('/cim_two_voice', methods=["GET", "POST"])
+@jwt_required()
+def cim_two_voice():
+
+	jsonData = json.dumps(request.json)
+	target = json.loads(jsonData)
+	target_name = target['username']
+
+	flag = ft_cim_two_voice(current_identity.id, target_name)
+
+	if flag[0] and flag[1]:
+		if flag[1] == -1:
+			if flag[0] == 1:
+				return make_response(jsonify({
+					'flag': 'userの移動が完了した旨のフラグ'
+				}))
+			else:
+				return make_response(jsonify({
+					'flag': 'userの招待が完了した旨のフラグ'
+				}))
+		elif flag[1] == 1:
+			if flag[0] == 1:
+				return make_response(jsonify({
+					'flag': 'チャンネルの作成と、2人のuserの移動が完了した旨のフラグ'
+				}))
+			else:
+				return make_response(jsonify({
+					'flag': 'チャンネルの作成と、userの招待、相手の移動が完了した旨のフラグ'
+				}))
+		else:
+			if flag[0] == 1:
+				return make_response(jsonify({
+					'flag': 'チャンネルの作成と、userの移動、相手の正体が完了した旨のフラグ'
+				}))
+			else:
+				return make_response(jsonify({
+					'flag': 'チャンネルの作成と、２人のuserの招待が完了した旨のフラグ'
+				}))
+	else:
+		return make_response(jsonify({
+			'flag': 'エラーが起こった旨のフラグ'
+	}))
+
 
 @discordbot.route('/status', methods=["GET", "POST"])
+@jwt_required()
+def return_status():
+
+	jsonData = json.dumps(request.json)
+	target = json.loads(jsonData)
+	target_name = target['username']
+
+	flag = ft_return_status(target_name)
+	if flag:
+		return make_response(jsonify({
+			'flag': 'userがオンラインである旨のフラグ'
+		}))
+	else:
+		return make_response(jsonify({
+			'flag': 'userがオンラインでない旨のフラグ'
+		}))
+
 
 @discordbot.route('/move_freevoice', methods=["GET", "POST"])
-
-@discordbot.route('/delete_channel', methods=["GET", "POST"])
+@jwt_required()
+def move_freevoice():
+	flag = ft_move_freevoice(current_identity.id)
+	if flag:
+		return make_response(jsonify({
+			'flag': 'userをFreeVoiceに移動した旨のフラグ'
+		}))
+	else:
+		return make_response(jsonify({
+			'flag': 'userをFreeVoiceに移動できなかった旨のフラグ'
+		}))
